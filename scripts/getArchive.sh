1e82dd835d56dc83c01e9b722e8047449c0a5890
tigercosmos <phy.tiger@gmail.com>
2018-05-05 20:58:46 +0800

    time format and enter virtual env

--- scripts/getArchive.sh
+++ scripts/getArchive.sh
@@ -4,17 +4,21 @@ URL=$2
 if [ -z $2 ]:
 then
     echo "Not correct argument supplied"
-    echo "sh ./getArchive.sh NAME URL"
+    echo "sh ./getArchive.sh NAME \"URL\""
 else
+    # enter virtual environment
+    source warcm_env/virt1/bin/activate
+    # submodule WarcMiddleware
     cd WarcMiddleware
     python crawler.py --url $URL
-    EPOCH=$(date +%s)
+    EPOCH=$(date +"%Y-%m-%d")
     FILENAME="$EPOCH.warc.gz"
     mv ./out.warc.gz $FILENAME
     cd ..
     if [ ! -d "collections/$NAME" ]; then
         wb-manager init $NAME
     fi
+    # save in archive
     wb-manager add $NAME "./WarcMiddleware/$FILENAME"
     rm "./WarcMiddleware/$FILENAME"
 fi
